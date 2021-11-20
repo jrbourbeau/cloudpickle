56053c45ffacc203632437455ee0029bb3bd4877
James Bourbeau <jrbourbeau@gmail.com>
2021-11-19 18:45:24 -0600

    Don't attempt _pickle import on PYPY

--- cloudpickle/compat.py
+++ cloudpickle/compat.py
@@ -3,7 +3,7 @@ import sys
 
 PYPY = platform.python_implementation() == "PyPy"
 
-if sys.version_info < (3, 8):
+if sys.version_info < (3, 8) or PYPY:
     try:
         import pickle5 as pickle  # noqa: F401
         from pickle5 import Pickler  # noqa: F401
